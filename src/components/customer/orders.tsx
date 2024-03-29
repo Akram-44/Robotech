8e3c91e9a50cdaae3cf2b7eb4763dbc42f9cd386
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-03-29 15:56:52 +0000

    Update

--- src/components/customer/orders.tsx
+++ src/components/customer/orders.tsx
@@ -1,10 +1,16 @@
 import { getCustomerOrders } from "@/supabase/getCustomerOrders";
 import { CourseType, OrderType } from '../../../type'
 import { useEffect, useState } from "react";
-import { Loader } from "lucide-react";
+import { ChevronRight, Loader, Plus } from "lucide-react";
 
 const Orders = ({ customerId }) => {
-    const [ordersList, setOrdersList] = useState<OrderType | null>(null)
+    const [ordersList, setOrdersList] = useState<OrderType | null>(null);
+    const [collapse, setCollapse] = useState({
+        products: false,
+        courses: false,
+        services: false
+    });
+
     useEffect(() => {
         async function fetchData() {
             try {
@@ -18,52 +24,53 @@ const Orders = ({ customerId }) => {
         }
         fetchData().then(data => setOrdersList((data! as OrderType)));
     }, []);
-    return <div className="h-[300px] border overflow-auto rounded mt-3 p-3">
+
+    const toggleCollapse = (category) => {
+        setCollapse(prevState => ({
+            ...prevState,
+            [category]: !prevState[category]
+        }));
+    };
+    const placeOrder = (category) => {
+        console.log(category)
+    }
+    const renderItems = (items, title, imageField, noItemsMessage, category) => (
+        <div>
+            <h2 onClick={() => toggleCollapse(category)} className='flex items-center mt-2 font-bold mb-1 text-blue-600 text-base cursor-pointer'><span>{title.toUpperCase()}</span> <ChevronRight className="ml-2" size={16} /></h2>
+            {collapse[category] && <>
+                <Plus onClick={() => placeOrder(category)} className="ml-auto bg-blue-500 text-white rounded-full p-1 my-2" />
+                <ul>
+                    {items ? (
+                        items.map(item => (
+                            <li className="flex items-center justify-between gap-2 bg-slate-200 rounded p-3" key={item.id}>
+                                <div className="flex items-center gap-1">
+                                    <img className="w-10 h-10 rounded-full" src={item[imageField]} alt={item.title} />
+                                    <h3>{item.title}</h3>
+                                </div>
+                                <div>Price: ${item.price}</div>
+                            </li>
+                        ))
+                    ) : (
+                        <li>{noItemsMessage}</li>
+                    )}
+                </ul></>}
+        </div>
+    );
+
+    return <div className="h-[400px] border overflow-auto rounded mt-3 p-3">
+
         {ordersList ? (
             <div className="mt-2">
-                <h2>Products</h2>
-                <ul>
-                    {ordersList.products ? ordersList.products.map((product) => (
-                        <li className="flex items-center justify-between gap-2 bg-slate-200 rounded p-3 " key={product.id}>
-                            <div className="flex items-center gap-1">
-                                <img className="w-10 h-10 rounded-full" src={product.image1} alt={product.title} />
-                                <h3>{product.title}</h3>
-                            </div>
-                            <div>Price: ${product.price}</div>
-                        </li>
-                    )) : 'No products available'}
-                </ul>
-                <h2>Courses</h2>
-                <ul>
-                    {ordersList.courses ? ordersList.courses.map((course) => (
-                        <li className="flex items-center justify-between gap-2 bg-slate-200 rounded p-3 " key={course.id}>
-                            <div className="flex items-center gap-1">
-                                <img className="w-10 h-10 rounded-full" src={course.poster} alt={course.title} />
-                                <h3>{course.title}</h3>
-                            </div>
-                            <div>Price: ${course.price}</div>
-                        </li>
-                    )) : 'No courses available'}
-                </ul>
-                <h2>Services</h2>
-                <ul>
-                    {ordersList.services ? ordersList.services.map((service) => (
-                        <li className="flex items-center justify-between gap-2 bg-slate-200 rounded p-3 " key={service.id}>
-                            <div className="flex items-center gap-1">
-                                <img className="w-10 h-10 rounded-full" src={service.image1} alt={service.title} />
-                                <h3>{service.title}</h3>
-                            </div>
-                            <div>Price: ${service.price}</div>
-                        </li>
-                    )) : 'No services available'}
-                </ul>
+                {renderItems(ordersList.products, "Products", "image1", "No products available", "products")}
+                {renderItems(ordersList.courses, "Courses", "poster", "No courses available", "courses")}
+                {renderItems(ordersList.services, "Services", "image1", "No services available", "services")}
             </div>
         ) : (
             <div className="text-center h-full flex items-center justify-center">
-                <span className="flex gap-1 items-center"><Loader className="animate-spin" />  </span></div>
+                <span className="flex gap-1 items-center"><Loader className="animate-spin" />  </span>
+            </div>
         )}
+    </div>;
+};
 
-    </div>
-}
-
-export default Orders
+export default Orders;
